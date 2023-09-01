f08745c447789399062c6487895874dc186613af
Ihor Tonkovyd <142664318+ITonkovyd@users.noreply.github.com>
2023-09-01 09:12:31 +0300

    changing key value

--- src/components/Feedback/FeedbackOptions/FeedbackOptions.jsx
+++ src/components/Feedback/FeedbackOptions/FeedbackOptions.jsx
@@ -1,23 +1,29 @@
 import PropTypes from 'prop-types';
-import shortid from "shortid";
-import { FeedbackButton, FeedbackElement, FeedbackList } from "./FeedbackOptions.styled";
+import {
+  FeedbackButton,
+  FeedbackElement,
+  FeedbackList,
+} from './FeedbackOptions.styled';
 
 const FeedbackOptions = ({ options, onFeedbackClick, dispatch }) => (
-    <FeedbackList>
-      {options.map((option) => (
-        <FeedbackElement key={shortid.generate()}>
-          <FeedbackButton type='button' onClick={() => onFeedbackClick(option, dispatch)}>{option[0].toUpperCase() + option.slice(1)}</FeedbackButton>
-        </FeedbackElement>
-      ))}
+  <FeedbackList>
+    {options.map((option, index) => (
+      <FeedbackElement key={index}>
+        <FeedbackButton
+          type="button"
+          onClick={() => onFeedbackClick(option, dispatch)}
+        >
+          {option[0].toUpperCase() + option.slice(1)}
+        </FeedbackButton>
+      </FeedbackElement>
+    ))}
+  </FeedbackList>
+);
 
-    </FeedbackList>
+export default FeedbackOptions;
 
-  )
-
-export default FeedbackOptions
-  
 FeedbackOptions.propTypes = {
   options: PropTypes.arrayOf(PropTypes.string.isRequired).isRequired,
   onFeedbackClick: PropTypes.func.isRequired,
   dispatch: PropTypes.func,
-}
+};
--- src/components/Feedback/FeedbackOptions/FeedbackOptions.styled.jsx
+++ src/components/Feedback/FeedbackOptions/FeedbackOptions.styled.jsx
@@ -1,21 +1,21 @@
-import styled from "styled-components";
+import styled from 'styled-components';
 
 export const FeedbackList = styled.ul`
-display: flex; 
-list-style: none;
-padding-left: 0;
-`
+  display: flex;
+  list-style: none;
+  padding-left: 0;
+`;
 
 export const FeedbackElement = styled.li`
-margin-right: 10px;
-`
+  margin-right: 10px;
+`;
 
 export const FeedbackButton = styled.button`
   font-weight: 600;
   border: 1px solid gray;
   border-radius: 4px;
   cursor: pointer;
-  :hover{
-    background-color: #4040b8;
+  :hover {
+    background-color: #6fadc1;
   }
-`
+`;
